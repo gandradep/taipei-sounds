698a349ca8be745e655839872fe633c60e13584a
gandradep <eventsonido@gmail.com>
2023-04-26 22:23:14 +0800

    Playing sound in sketch

--- src/components/Inter.js
+++ src/components/Inter.js
@@ -1,17 +1,24 @@
 /* eslint-disable */
 import React from 'react';
 import Sketch from 'react-p5';
+import 'p5/lib/addons/p5.sound';
 
 const Inter = () => {
   const x = 50;
   const y = 50;
   let img;
+  let mySound;
   const preload = (p5) => {
     img = p5.loadImage('./assets/busScratch.jpg');
+    p5.soundFormats('mp3', 'ogg');
+    mySound = p5.loadSound('./assets/mrtRemixV2.mp3');
   };
   const setup = (p5, parent) => {
-    p5.createCanvas(810, 540).parent(parent);
-    p5.background(0);
+    const cnv = p5.createCanvas(810, 540).parent(parent);
+    cnv.mousePressed(() => {
+      mySound.play();
+    })
+    img.resize(p5.width, p5.height);
   };
   const draw = (p5) => {
     p5.rect(0, 0, p5.width, p5.height);
