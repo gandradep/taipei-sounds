44d17d9f6eb0ef63def86394c0c8102d4f827dc3
gandradep <eventsonido@gmail.com>
2023-05-15 22:00:18 +0800

    Add original songs and logic to switch array when playing.

--- src/components/P5sketch.js
+++ src/components/P5sketch.js
@@ -11,6 +11,9 @@ const P5sketch = (props) => {
   let xXtra = 0 ;
   let busBg, mrtBg, gBg, bus, gTruck, mrt;
   let mrtRemix, busRemix, garRemix, currentSong;
+  let busSong, mrtSong, garbageSong;
+  let songs0;
+  let songsRemix ;
   const preload = (p5) => {
     busBg = p5.loadImage('./assets/busBg.jpg');
     gBg = p5.loadImage('./assets/gBg.jpg');
@@ -22,41 +25,37 @@ const P5sketch = (props) => {
     mrtRemix = p5.loadSound('./assets/mrtRemixV2.mp3');
     busRemix = p5.loadSound('./assets/busRemix.mp3');
     garRemix = p5.loadSound('./assets/garRemix.mp3');
-
-
+    busSong = p5.loadSound('./assets/busSong.mp3');
+    garbageSong = p5.loadSound('./assets/garbageSong.mp3');
+    mrtSong = p5.loadSound('./assets/mrtSong.mp3');
   };
   const setup = (p5, parent) => {
     const vehicle = [bus, mrt, gTruck];
     const bgImage = [busBg, mrtBg, gBg];
-    const songsRemix = [busRemix, mrtRemix, garRemix];
-    console.log(songsRemix);
+    songsRemix = [busRemix, mrtRemix, garRemix];
+    songs0 = [busSong, mrtSong, garbageSong];
+
     p5.createCanvas(810, 540).parent(parent);
-    // cnv.mousePressed(() => {
-    //   mySound.play();
-    // })
     for (let i = 0; i< bgImage.length; i++){
       bgImage[i].resize(p5.width, p5.height );
     }
     for (let i = 0; i< vehicle.length; i++){
       vehicle[i].resize(p5.width*0.25, p5.height*0.20);
     }
-    test.current = new Transport(vehicle, bgImage, songsRemix, p5.width*0.1, p5.height*0.69);
+    test.current = new Transport(vehicle, bgImage, p5.width*0.1, p5.height*0.69, songsRemix, songs0);
   };
   const draw = (p5) => {
     p5.noFill();
     p5.rect(0, 0, p5.width, p5.height);
-    test.current.show(p5, count, xXtra, yxtra);
-    test.current.playSound(count, playCheck);
     angle += 0.02;
     yxtra = 5 * Math.sin(angle);
     xXtra = posCheck ? p5.width/2 : 0;
-
+    test.current.show(p5, count, xXtra, yxtra);
+    test.current.playSound(count, playCheck);
   };
 
   const mousePressed = (p5) => {
-
-    // currentSong.stop();
-    // currentSong.play();
+    // console.log(currentSong)
   }
 
   return (
--- src/components/Transport.js
+++ src/components/Transport.js
@@ -1,36 +1,46 @@
 class Transport {
-  constructor(img, bgImg, songR, x, y) {
+  constructor(img, bgImg, x0, y, songMix, song0) {
     this.img = img;
     this.bgImg = bgImg;
-    this.songR = songR;
     this.y = y;
-    this.x = x;
+    this.x0 = x0;
+    this.x = 0;
     this.var = 0;
     this.isPlaying = false;
+    this.songMix = songMix;
+    this.song0 = song0;
+    this.current = [];
   }
 
   show(p5, num, xXtra, xtra) {
+    this.x = this.x0 + xXtra;
     p5.image(this.bgImg[num], 0, 0);
-    p5.image(this.img[num], this.x + xXtra, this.y + xtra);
+    p5.image(this.img[num], this.x, this.y + xtra);
+
   }
 
   playSound(num, pCheck) {
-
+    this.current = (this.x === this.x0) ? this.song0 : this.songMix;
+    console.log(this.x);
+    // console.log(this.x);
+    // console.log(this.current);
     if (pCheck) {
-      console.log('playing:'+num);
+      if (this.var !== num) {
+        // this.songR[this.var].stop();
+        console.log('Next stop:' + this.current[this.var].url);
+        this.var = num;
+        this.isPlaying = pCheck;
+        // this.songR[num].play();
+        console.log('NExtplaying:' + this.current[this.var].url);
+      }
+      if (this.isPlaying !== pCheck) {
+        console.log('playing:' + this.current[this.var].url);
+        this.isPlaying = pCheck;
+      }
     } else {
-      console.log('stop:'+num);
+      console.log('stop:' + this.current[this.var].url );
+      this.isPlaying = pCheck;
     }
-    // if (this.var !== num) {
-    //   if (this.var >= 0) {
-    //     this.songR[this.var].stop();
-    //   }
-    //   this.var = num;
-    //   this.songR[num].play();
-    // }
-  }
-  checkTrack(num) {
-
   }
 }
 
